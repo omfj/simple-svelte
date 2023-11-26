d3b5d444cf9feaff42492cb293c21fb0e22157bb
Ole Magnus Fon Johnsen <me@omfj.no>
2023-11-26 03:22:04 +0100

    update drizzle

--- src/lib/db/drizzle.ts
+++ src/lib/db/drizzle.ts
@@ -1,27 +1,27 @@
-import { DATABASE_URL } from "$env/static/private";
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schemas";
+import { DATABASE_URL } from '$env/static/private';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schemas';
 
 const globalForPg = globalThis as unknown as {
-  pg: postgres.Sql | undefined;
+	pg: postgres.Sql | undefined;
 };
 
 let pg;
 
-if (process.env.NODE_ENV !== "production") {
-  if (!globalForPg.pg) {
-    globalForPg.pg = createPool();
-  }
-  pg = globalForPg.pg;
+if (process.env.NODE_ENV !== 'production') {
+	if (!globalForPg.pg) {
+		globalForPg.pg = createPool();
+	}
+	pg = globalForPg.pg;
 } else {
-  pg = createPool();
+	pg = createPool();
 }
 
 function createPool() {
-  return postgres(DATABASE_URL);
+	return postgres(DATABASE_URL);
 }
 
 export const db = drizzle(pg, {
-  schema,
+	schema,
 });
